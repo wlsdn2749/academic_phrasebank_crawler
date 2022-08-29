f8c115676415cd498bc5faa22b4c2cd1954a16bd
Jinwoo Jung <wlsdn2749@naver.com>
2022-08-29 15:59:25 +0900

    unicode error solved, v0.2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cmath import e
 import enum
 from re import sub
 import requests
@@ -46,8 +47,6 @@ def phraseBank_parser(url_name):
                 td_lst = []
                 for i in td:
                     td_lst.append(i.find_all(text=True))
-                # td_fst = td[0].find_all(text=True)
-                # td_sec = td[1].find_all(text=True)
             except:
                 pass
 
@@ -59,48 +58,67 @@ def phraseBank_parser(url_name):
             data = {'title': title
 
             }
-            for p_tag_idx, p_tag in enumerate(p_lst):
+            
+            toggle = False
+            nbsp = "\xa0"
+
+            real_p_lst = []
+            real_real_p_lst = []
+            temp_data = ""
+
+            for p_list in p_lst:
+                for p_tag in p_list:
+                    if p_tag.startswith(nbsp):
+                        if p_tag == nbsp:
+                            continue
+                        real_p_lst[-1] += p_tag.lstrip(nbsp)
+                        continue
+                    if p_tag.endswith(nbsp):
+                        if p_tag == nbsp:
+                            continue
+                        temp_data += p_tag.rstrip(nbsp)
+                        continue
+                    temp_data += p_tag
+                    real_p_lst.append(temp_data)
+                    temp_data = ""
+                real_real_p_lst.append(real_p_lst)
+
+            for p_tag_idx, p_tag in enumerate(real_real_p_lst):
                 for idx, p_t in enumerate(p_tag):
+                    # if p_t.startswith(nbsp) and idx >= 2:
+                    #     data[f'p{p_tag_idx}-{idx-2}'] = data[f'p{p_tag_idx}-{idx-1}'] + p_t.lstrip(nbsp)
+                    #     continue
+                    # if p_t.endswith(nbsp):
+                    #     data[f'p{p_tag_idx}-{idx}'] = p_t.rstrip(nbsp)
+                    #     toggle = True
+                    #     continue
+                    # if toggle == True:
+                    #     data[f'p{p_tag_idx}-{idx-1}'] = data[f'p{p_tag_idx}-{idx-1}'] + p_t
+                    #     toggle = False
+                    #     continue
+                    if p_t.startswith("\n"):
+                        p_t = p_t.lstrip("\n")
                     data[f'p{p_tag_idx}-{idx}'] = p_t
 
             for td_tag_idx, td_tag in enumerate(td_lst):
                 for idx, td_t in enumerate(td_tag):
+                    if td_t.startswith("\n"):
+                        td_t = td_t.lstrip("\n")
                     data[f'td{td_tag_idx}-{idx}'] = td_t    
             res.append(data)
 
     with open (f'{url_name}.json', 'w', encoding='utf-8') as f:
         json.dump(res, f, indent=8, ensure_ascii=False)
     print(f"Created Json File {url}")
-# subtitles = soup.find_all('div', attrs={'class': 'et_pb_row_inner et_pb_row_inner_1'})
 
-# for subtitle in subtitles:
-#     titles = subtitle.find_all('h5', attrs={'class': 'et_pb_toggle_title'})
-#     datas = subtitle.find_all('p')
-#     data_set = []
-#     p = re.compile('\W')
 
-#     for idx, data in enumerate(datas):
-#         real_data = data.find_all(text=True)
-#         if p.match(real_data[0]) == None:
-#             data_set.append(real_data)
-    
-#     for title, data in zip(titles, data_set):
-#         print("title is = " , title.text)
-#         print("data is = ", data)
-
-        
-
-    
-    # print(title + "\n", data)
-
-    #et_pb_toggle et_pb_module et_pb_accordion_item et_pb_accordion_item_1 et_pb_toggle_open
-    #et_pb_toggle et_pb_module et_pb_accordion_item et_pb_accordion_item_0 et_pb_toggle_open
-
-    
 if __name__ == "__main__":
     phraseBank_list = ["introducing-work", "referring-to-sources", "describing-methods",
                        "reporting-results", "discussing-findings", "writing-conclusions"]
 
     for page in phraseBank_list:
         phraseBank_parser(page)
-    
+    # dummy = '\xa0for all variables.\n'
+    # print (dummy.startswith("\xa0"))
+    # print (dummy.lstrip("\xa0"))
+    # print (dummy.lstrip("\xa0").rstrip("\n"))
